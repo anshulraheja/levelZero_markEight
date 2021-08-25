a9e231cb3e7713164042c84af015333dcd84d117
Anshul Raheja <40492015+anshulraheja@users.noreply.github.com>
2021-08-26 04:10:25 +0530

    style : add styling to the code

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# levelZero_markEight
-Created with CodeSandbox
+# Emoticon Interpreter
+
+## Description
+
+###### (levelZero_markEight)
+
+The interpreter displays the name of the emoji entered by the user manually or selected from the list of Emojis
+The Emojis are present as an array of objects in the asset folder.
+
+### My learnings
+
+useRef hook is used to directly create a reference to the DOM element in the functional component. The useRef returns a mutable object. The value is persisted and can be accessed from the current property of the returned object. The object can persist a value for a full lifetime of the component.
+
+#### Tech Stack
+
+React, CSS
+
+## Installation
+
+```bash
+git clone
+git install
+```
--- src/components/EmojiInterpreter.js
+++ src/components/EmojiInterpreter.js
@@ -1,31 +1,32 @@
 import emojiData from "/assets/emojiData";
 import { useRef, useState } from "react";
-import "./EmojiInterpreter.css";
 
 export default function EmojiInterpreter() {
   //state variable to store emoji and it's meaning
-  const [emoji, setEmoji] = useState();
+  const [emoji, setEmoji] = useState("");
   const [meaning, setMeaning] = useState();
 
   //variable to toggle emoji section
   const [viewSection, setViewSection] = useState(false);
 
   //variable to hide default meaning if no input present
-  const userInput = useRef();
+  const userInput = useRef("");
 
   //function to match user input with our emoji database
   const userInputChangeHandler = (e) => {
+    const InputLength = userInput.current.value.length;
     const InpuEmoji = e.target.value;
     setEmoji(InpuEmoji);
-
     const foundData = emojiData.find((e) => e.emoji === InpuEmoji);
 
     if (foundData) {
       setMeaning(foundData.description);
     } else {
-      if (userInput.length > 0)
-        setMeaning("This emoji is not availabe in our Database");
-      else setMeaning();
+      if (InputLength > 0) {
+        setMeaning("This emoji is not availabe in our database");
+      } else {
+        setMeaning("");
+      }
     }
   };
 
@@ -33,26 +34,34 @@ export default function EmojiInterpreter() {
   function emojiClickHandler(emo) {
     setMeaning(emo.description);
   }
+
   return (
-    <div>
-      <h1>Welcome to Emoji Interpreter</h1>
-      <input
-        ref={userInput}
-        onChange={userInputChangeHandler}
-        placeholder="Enter a Emoji"
-        value={emoji}
-      />
-      <h3>{meaning}</h3>
-      <button onClick={() => setViewSection(!viewSection)}>
-        {viewSection === true ? "Hide Emojis Section" : "Show Emojis Section"}
+    <div className="container">
+      <h1 className="heading">Emoticon Interpreter</h1>
+      <div className="center-container">
+        <input
+          className="input"
+          ref={userInput}
+          onChange={userInputChangeHandler}
+          placeholder="Enter a Emoji"
+          value={emoji}
+        />
+        <h3 className="meaning">{meaning}</h3>
+      </div>
+      <button
+        className="show-button"
+        onClick={() => setViewSection(!viewSection)}
+      >
+        {viewSection === true ? "Hide Emojis" : "Show Emojis"}
       </button>
       {viewSection === true ? (
-        <div>
+        <div className="emojis-container">
           {emojiData.map((emo) => (
             <span
+              style={{ fontSize: "2rem", cursor: "pointer" }}
+              className="emoji"
               key={emo.emoji}
               onClick={() => emojiClickHandler(emo)}
-              style={{ fontSize: "2rem", padding: "0.5rem", cursor: "pointer" }}
             >
               {emo.emoji}
             </span>
